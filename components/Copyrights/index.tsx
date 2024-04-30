6624169596545644ceb168c6f70fbe12cf8cb5ff
ir3n <iren.borada@gmail.com>
2024-04-30 19:01:49 +0300

    Fixes the date and link of the copyrights

--- components/Copyrights/index.tsx
+++ components/Copyrights/index.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 const Copyrights = () => {
   return (
     <div className="p-[60px] text-center text-sm font-light">
-      <p className="mb-[10px]">© 2024 All rights reserved</p>
+      <p className="mb-[10px]">{`© ${new Date().getFullYear()} All rights reserved`}</p>
       <p className="">
         Designed by Thanassis Mavrakis. Coded by{" "}
         <Link
-          href="ireneborada.com"
+          href="https://www.ireneborada.com/"
           target="_blank"
           className="hover:text-accent transition duration-500"
         >
