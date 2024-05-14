c81efcf423eee931774833b4f94bf87671984613
ir3n <iren.borada@gmail.com>
2024-05-14 18:05:35 +0300

    Changes gsap implementation for contact box

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
+import { usePathname } from "next/navigation";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
 
 import SocialLink from "./SocialLink";
 import CircleButton from "../CircleButton";
@@ -11,29 +11,46 @@ import CircleButton from "../CircleButton";
 import data from "@/data/footer.json";
 
 if (typeof window !== "undefined") {
-  gsap.registerPlugin(ScrollTrigger, useGSAP);
+  gsap.registerPlugin(ScrollTrigger);
 }
 
 const Contact = () => {
   const { subtitle, title, button, social } = data;
+  const path = usePathname();
 
-  useGSAP(() => {
-    gsap.to(".inner", {
-      scale: 1,
-      borderRadius: 0,
-      scrollTrigger: {
-        trigger: ".box",
-        start: "center bottom",
-        end: "bottom bottom",
-        scrub: true,
-      },
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      let timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".box",
+          start: "center bottom",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      });
+
+      timeline.fromTo(
+        ".inner",
+        {
+          width: "200px",
+          height: "200px",
+          borderRadius: "50%",
+        },
+        {
+          width: "100%",
+          height: "100%",
+          borderRadius: "14px",
+        }
+      );
     });
-  });
+
+    return () => ctx.revert();
+  }, [path]);
 
   return (
     <div className="relative">
       <div className="box absolute top-0 left-[20px] lg:left-[50px] right-[20px] lg:right-[50px] bottom-0 rounded overflow-hidden flex items-center justify-center">
-        <div className="inner md:w-full h-full md:h-auto aspect-square bg-secondary backdrop-blur scale-[10%] origin-center rounded-full"></div>
+        <div className="inner bg-secondary backdrop-blur origin-center"></div>
       </div>
       <div className="container py-[40px] lg:py-[110px] relative z-1">
         <div className="flex justify-between gap-20 px-[20px] md:px-[0] relative pb-[150px] md:pb-0">
--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function Logo() {
   return (
-    <div className="max-w-[62px] md:max-w-[85px] relative z-50">
+    <div className="block max-w-[62px] md:max-w-[85px] relative z-50 transition duration-500 hover:scale-[1.1]">
       <Link href="/">
         <Image
           src="/images/logo.svg"
